6f70d886fa15eb8403bf37e8792fd451435ad7d8
Emma-Winter-Pub <zggzklwmg@gmail.com>
2026-06-11 12:11:25 +0800

    Delete src directory

--- src/GlobalFftwLock.cpp
+++
@@ -1,6 +0,0 @@
-#include "GlobalFftwLock.h"
-
-std::recursive_mutex& getGlobalFftwLock() {
-    static std::recursive_mutex s_globalFftwMutex;
-    return s_globalFftwMutex;
-}
--- src/GlobalFftwLock.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBAL_FFTW_LOCK_H
-#define GLOBAL_FFTW_LOCK_H
-
-#include <mutex>
-
-std::recursive_mutex& getGlobalFftwLock();
-
-#endif
